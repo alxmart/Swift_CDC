342ce8b1f0a4e6398cbda65f54dc65e0647648a3
alxmart <alxmartinez@gmail.com>
2024-07-12 18:03:10 -0300

    Testing Xcode Swift Playground

---
+++ LearnSwift_Introduction.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//var greeting = "Hello, playground"
+var greeting = "Hello World - Swift !!!"
+
+//-----------------------------
+// Codecademy - Learn Swift
+//-----------------------------
+
+// Hello
+print(greeting)
+
+// 
